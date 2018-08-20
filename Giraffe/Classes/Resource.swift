9e58f51aa9d6279ec020202e41d47960be2a8847
Derek <derekcoder@gmail.com>
2018-08-20 15:07:13 +0800

    Add headers for all resource inits

--- Giraffe/Classes/Resource.swift
+++ Giraffe/Classes/Resource.swift
@@ -44,19 +44,20 @@ public struct Resource<A> {
     public var parse: (Data, HTTPURLResponse) -> Result<A>
     public var headers: [String: String]? = nil
     public var timeoutInterval: TimeInterval = 20.0 // in seconds, default: 60 seconds
-    
+}
+
+extension Resource {
     public init(url: URL, method: HttpMethod<Data?> = .get, headers: [String: String]? = nil, parse: @escaping (Data, HTTPURLResponse) -> Result<A>) {
         self.url = url
         self.parse = parse
         self.method = method
         self.headers = headers
     }
-}
 
-extension Resource {
-    public init(url: URL, method: HttpMethod<Data?> = .get , parseJSON: @escaping (Any, HTTPURLResponse) -> Result<A>) {
+    public init(url: URL, method: HttpMethod<Data?> = .get, headers: [String: String]? = nil, parseJSON: @escaping (Any, HTTPURLResponse) -> Result<A>) {
         self.url = url
         self.method = method
+        self.headers = headers
         self.parse = { data, response in
             guard let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) else {
                 return Result.error(WebserviceError.jsonParsingFailed)
@@ -65,19 +66,21 @@ extension Resource {
         }
     }
     
-    public init(url: URL, jsonMethod: HttpMethod<Any>, parse: @escaping (Data, HTTPURLResponse) -> Result<A>) {
+    public init(url: URL, jsonMethod: HttpMethod<Any>, headers: [String: String]? = nil, parse: @escaping (Data, HTTPURLResponse) -> Result<A>) {
         self.url = url
         self.parse = parse
+        self.headers = headers
         self.method = jsonMethod.map { jsonObject in
             try! JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions())
         }
     }
     
-    public init(url: URL, jsonMethod: HttpMethod<Any>, parseJSON: @escaping (Any, HTTPURLResponse) -> Result<A>) {
+    public init(url: URL, jsonMethod: HttpMethod<Any>, headers: [String: String]? = nil, parseJSON: @escaping (Any, HTTPURLResponse) -> Result<A>) {
         self.url = url
         self.method = jsonMethod.map { jsonObject in
             try! JSONSerialization.data(withJSONObject: jsonObject, options: JSONSerialization.WritingOptions())
         }
+        self.headers = headers
         self.parse = { data, response in
             guard let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) else {
                 return Result.error(WebserviceError.jsonParsingFailed)
