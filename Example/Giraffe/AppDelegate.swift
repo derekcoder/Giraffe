700d37fe8b4ad73aabb69f789cc4c92c0228eb82
DerekCoder <derekcoder@gmail.com>
2018-12-11 16:23:54 +0800

    Upgrade to Swift 4.2

--- Example/Giraffe/AppDelegate.swift
+++ Example/Giraffe/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     private(set) var app: App?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if let window = window {
             app = App(window: window)
         }
